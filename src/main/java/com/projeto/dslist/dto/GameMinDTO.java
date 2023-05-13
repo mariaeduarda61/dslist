58dc56e17af6632a0830e40311526789cdc87851
Maria Eduarda <mariaeduardavieira061@gmail.com>
2023-05-13 20:16:26 -0300

    Deploy ready

--- src/main/java/com/projeto/dslist/dto/GameMinDTO.java
+++ src/main/java/com/projeto/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.projeto.dslist.dto;
 
 import com.projeto.dslist.entities.Game;
+import com.projeto.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -20,6 +21,15 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription =entity.getShortDescription();
+	}
+	
+
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year =projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription =projection.getShortDescription();
 	}
 
 	public Long getId() {
--- src/main/java/com/projeto/dslist/services/GameService.java
+++ src/main/java/com/projeto/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.projeto.dslist.dto.GameDTO;
 import com.projeto.dslist.dto.GameMinDTO;
 import com.projeto.dslist.entities.Game;
+import com.projeto.dslist.projections.GameMinProjection;
 import com.projeto.dslist.repositories.GameRepository;
 
 @Service
@@ -22,11 +23,19 @@ public class GameService {
 		return new GameDTO(result);
 	}
 	
+	
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
+	
+    }
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+       return result.stream().map(x -> new GameMinDTO(x)).toList();
 	
     }
 }
